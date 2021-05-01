b9daf9e72da4383c7508406c3046efff78b7735d
Andrii Nykyforuk <a01247560@unet.univie.ac.at>
2021-05-01 22:41:27 +0200

    add Customer.class

---
+++ ShopApp/src/duke/choice/Customer.java
@@ -0,0 +1,13 @@
+package duke.choice;
+
+public class Customer {
+    private String name;
+
+    public Customer(String _name){
+        this.name = _name;
+    }
+
+    public void printName(){
+        System.out.println(this.name);
+    }
+}
--- ShopApp/src/duke/choice/ShopApp.java
+++ ShopApp/src/duke/choice/ShopApp.java
@@ -3,5 +3,9 @@ package duke.choice;
 public class ShopApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Duke Choice Shop!");
+
+        Customer c1 = new Customer("Pinky");
+        c1.printName();
+
     }
 }
