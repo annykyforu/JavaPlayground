500a8dff7d71ceff91f88f09525674ef7eb75bd9
Andrii Nykyforuk <a01247560@unet.univie.ac.at>
2021-05-01 21:21:22 +0200

    clean up

--- 0-JavaFX-Basics/src/sample/Main.java
+++ 0-JavaFX-Basics/src/sample/Main.java
@@ -1,14 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.beans.Observable;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
