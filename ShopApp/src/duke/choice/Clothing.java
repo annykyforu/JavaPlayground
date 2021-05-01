176df30ffa1bec3a01f59abc1c49154e421a5d21
Andrii Nykyforuk <a01247560@unet.univie.ac.at>
2021-05-01 23:14:22 +0200

    handling Text and Numbers

---
+++ ShopApp/src/duke/choice/Clothing.java
@@ -0,0 +1,23 @@
+package duke.choice;
+
+public class Clothing {
+    private String description;
+    private double price;
+    private String size;
+
+    public Clothing(String desc, double p, String s){
+        this.description = desc;
+        this.price = p;
+        this.size = s;
+    }
+
+    public void printInfo(){
+        System.out.println(this.description + " " + this.price + " " + this.size);
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+
+}
--- ShopApp/src/duke/choice/ShopApp.java
+++ ShopApp/src/duke/choice/ShopApp.java
@@ -1,11 +1,26 @@
 package duke.choice;
 
 public class ShopApp {
+
+    static String roundOffTo2DecPlaces(double val){
+        return String.format("%.2f", val);
+    }
+
     public static void main(String[] args) {
+        double tax = 0.2;
+        double total;
+
         System.out.println("Welcome to Duke Choice Shop!");
 
         Customer c1 = new Customer("Pinky");
-        c1.printName();
+        Clothing item1 = new Clothing("Blue Jacket", 20.9, "S");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 
+        item1.printInfo();
+        item2.printInfo();
+
+        total = (item1.getPrice() + item2.getPrice()*2);
+        total *= (1 + tax);
+        System.out.println("Total for 3 items: " + roundOffTo2DecPlaces(total));
     }
 }
